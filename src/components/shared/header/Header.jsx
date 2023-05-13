3b43fe79bf1b9132266a15a6651918e9124afeda
Ridoy Sharif <developersridoy@gmail.com>
2023-05-13 08:42:53 +0600

    install react and create some file tailwind taisyui added

---
+++ src/components/shared/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../../../assets/logo-1.png'
+
+const Header = () => {
+  return (
+    <div className='bg-transparent py-5'>
+        <div className='container mx-auto'>
+            <div className='flex justify-between items-center'>
+                <div>
+                    <img className='w-32 bg-white' src={Logo} alt="" />
+                </div>
+                <div className='flex gap-6 items-center'>
+                    <Link className='text-base font-medium text-black' to={`/`}>Home</Link>
+                    <Link className='text-base font-medium text-black' to={`/news`}>News</Link>
+                    <Link className='text-base font-medium text-black' to={`/destination`}>Destination</Link>
+                    <Link className='text-base font-medium text-black' to={`/`}>Blog</Link>
+                    <Link className='text-base font-medium text-black' to={`/contact`}>Contact</Link>
+                    {/* {user ? <>
+                        <p className='text-base text-black font-medium'>{user.displayName}</p>
+                        <Link className='text-base font-medium bg-[#F9A51A] text-black px-4 py-1 rounded-md'>Logout</Link>
+                    </>:
+                    <Link className='text-base font-medium bg-[#F9A51A] text-black px-4 py-1 rounded-md' to={`/login`}>Login</Link>
+                    } */}
+                </div>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
