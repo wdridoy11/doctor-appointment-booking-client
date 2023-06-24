8a4e3db500d3c0d3e36890b82483b9cbe90ba441
Ridoy Sharif <developersridoy@gmail.com>
2023-06-24 21:08:13 +0600

    Subscribe section creating

---
+++ src/pages/home/subscribe/Subscribe.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Subscribe = () => {
+  return (
+    <div>
+        <div className='container mx-auto bg-[#40d0c6]'>
+            <div>
+                <div>
+                    <h1>Subscribe</h1>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Subscribe
