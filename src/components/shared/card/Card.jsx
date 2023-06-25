2a779689e3b0b75686ce2e6bcf627ea4aa5e54e9
Ridoy Sharif <developersridoy@gmail.com>
2023-06-25 11:09:38 +0600

    blog card design continue

--- src/components/shared/card/Card.jsx
+++ src/components/shared/card/Card.jsx
@@ -3,8 +3,14 @@ import blogs from '../../../assets/banner-04.jpg'
 const Card = ({blog}) => {
     const {image}=blog;
   return (
-    <div>
-        <img src={blogs} alt="" />
+    <div className=''>
+        <img src={blogs} alt="Blogs" />
+        <div className='border border-slate-200 p-5 rounded-b-md'>
+            <h3 className='text-2xl font-semibold text-black mb-3'>Bangladesh is our country</h3>
+            <p className='text-base font-normal text-slate-500 mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptate fuga quos nulla provident dolore a obcaecati soluta. Expedita minima voluptatibus ab totam laboriosam ea, nam delectus dolor et molestiae?</p>
+            <button className='text-base font-medium bg-[#40d0c6] text-white px-7 py-2 rounded-md hover:bg-black duration-500'>Read More</button>
+        </div>
+
     </div>
   )
 }
